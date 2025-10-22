cdee35009b9b11d77ca8f96c18890fc8527723bf
Thomas Mellenthin <github@dysternis.de>
2025-10-22 21:40:52 +0200

    Surface playback errors

--- Sources/DingDongBlaster/VideoPlaybackController.swift
+++ Sources/DingDongBlaster/VideoPlaybackController.swift
@@ -9,6 +9,7 @@ final class VideoPlaybackController {
     private lazy var windowDelegate = PlaybackWindowDelegate(onClose: { [weak self] in
         self?.stopPlayback()
     })
+    private var statusObservation: NSKeyValueObservation?
 
     func playStream(from url: URL) {
         let playerItem = AVPlayerItem(url: url)
@@ -17,6 +18,7 @@ final class VideoPlaybackController {
         prepareWindowIfNeeded()
         playerView?.player = player
         self.player = player
+        observeStatus(of: playerItem)
         player.play()
         bringWindowToFront()
     }
@@ -25,6 +27,7 @@ final class VideoPlaybackController {
         player?.pause()
         player = nil
         playerView?.player = nil
+        statusObservation = nil
     }
 
     private func prepareWindowIfNeeded() {
@@ -54,6 +57,34 @@ final class VideoPlaybackController {
         guard let window else { return }
         window.makeKeyAndOrderFront(nil)
         NSApp.activate(ignoringOtherApps: true)
+    }
+
+    private func observeStatus(of item: AVPlayerItem) {
+        statusObservation = item.observe(\.status, options: [.initial, .new]) { [weak self] playerItem, _ in
+            guard let self else { return }
+            switch playerItem.status {
+            case .readyToPlay:
+                break
+            case .failed:
+                Task { @MainActor in
+                    self.handlePlaybackFailure(item: playerItem)
+                }
+            case .unknown:
+                break
+            @unknown default:
+                break
+            }
+        }
+    }
+
+    private func handlePlaybackFailure(item: AVPlayerItem) {
+        let errorDescription = item.error?.localizedDescription ?? "Unknown error"
+        stopPlayback()
+        let alert = NSAlert()
+        alert.messageText = "Unable to start playback"
+        alert.informativeText = errorDescription
+        alert.alertStyle = .warning
+        alert.runModal()
     }
 }
 
